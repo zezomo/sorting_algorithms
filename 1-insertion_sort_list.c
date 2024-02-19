04ad1075792431174e0e34e4912e1c417f1c96af
zezomo <zezouniverse7@gmail.com>
2024-02-19 09:37:14 -0800

    task 1

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,43 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - Sorts a doubly linked list of integers in ascending order
+ *                       using Insertion sort algorithm
+ * @list: Pointer to a pointer to the head of the doubly linked list
+ */
+void insertion_sort_list(listint_t **list)
+{
+    listint_t *current, *temp;
+
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
+
+    current = (*list)->next;
+
+    while (current != NULL)
+    {
+        temp = current->prev;
+
+        while (temp != NULL && temp->n > current->n)
+        {
+            if (temp->prev != NULL)
+                temp->prev->next = current;
+            else
+                *list = current;
+
+            if (current->next != NULL)
+                current->next->prev = temp;
+
+            temp->next = current->next;
+            current->prev = temp->prev;
+            current->next = temp;
+            temp->prev = current;
+
+            print_list(*list);
+            temp = current->prev;
+        }
+
+        current = current->next;
+    }
+}
+
