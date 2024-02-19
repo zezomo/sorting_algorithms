a17d55fcc6c0aa11c2c20ec3811d45c1c89a3095
zezomo <zezouniverse7@gmail.com>
2024-02-19 10:20:23 -0800

    task3

--- sort.h
+++ sort.h
@@ -30,9 +30,9 @@ void insertion_sort_list(listint_t **list);
 
 void selection_sort(int *array, size_t size);
 
+size_t lomuto_partition(int *array, ssize_t low, ssize_t high, size_t size);
+void quicksort(int *array, ssize_t low, ssize_t high, size_t size);
 void quick_sort(int *array, size_t size);
-void quick_sort_recursive(int *array, int low, int high, size_t size);
-int lomuto_partition(int *array, int low, int high, size_t size);
 
 
 
