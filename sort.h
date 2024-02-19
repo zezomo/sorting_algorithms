714b9afc33ad0b14962862ecfc91f1095c3268a1
zezomo <zezouniverse7@gmail.com>
2024-02-19 10:11:31 -0800

    task 3

--- sort.h
+++ sort.h
@@ -23,9 +23,16 @@ typedef struct listint_s
 
 void bubble_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
+
 void print_list(const listint_t *list);
+
 void insertion_sort_list(listint_t **list);
+
 void selection_sort(int *array, size_t size);
+
+void quick_sort(int *array, size_t size);
+void quick_sort_recursive(int *array, int low, int high, size_t size);
+int lomuto_partition(int *array, int low, int high, size_t size);
 
 
 
